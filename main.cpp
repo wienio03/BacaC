0a5ccdb4e5d10edbfa12b8708934b55543833da8
zmiana <wienczyslaw.wlodyga@student.uj.edu.pl>
2022-11-17 12:34:48 +0100

    zmiana 16_11 23:17

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 //Wieńczysław Włodyga
 #include <iostream>
+
+bool Universe(int x){
+    if (x<=4095 && x>=1) return true;
+    else return false;
+}
 void Cardinality(int set[], int* cardinality) {
     *(cardinality)=0;
     while (set[*cardinality]!=(-1)) {
             (*cardinality)++;
         }
-
+}
+void bubbleSort(int set[]) {
+    int size;
+    Cardinality(set, &size);
+    int i,j;
+    for (i=0; i<size; i++){
+        for (j=1; j<size; j++){
+            if (set[i]<set[j]) std::swap(set[i],set[j]);
+        }
+    }
 }
 void MinMax(int set[], int* min, int &max){
     int size;
@@ -31,28 +45,44 @@ void Add(int number, int set[]){
     set[n]=(-1);
     }
 }
-void Create(unsigned int usedElements, int sourceSet[], int set[]) {
-    int i;
-    for (i = 0; i < usedElements; i++) {
-        if (1 <= sourceSet[i] && sourceSet[i] <= 4095) {
-            set[i]=sourceSet[i];
-        } else {
-            usedElements++;
-            i++;
-        }
-    }
+void Create(unsigned int usedElements, int sourceSet[], int set[]){
+int i;
+for (i=0; i<usedElements; i++){
+    if(Universe(sourceSet[i]))Add(sourceSet[i],set);
 }
-    void Union(int set1[],int set2[], int setResultant[]){
-      int length1;
-      int length2;
-      Cardinality(set1,&length1);
-      Cardinality(set2,&length2);
-      int lengthUnion;
-      if(length1>length2) lengthUnion=length1;
-      if(length2>length1) lengthUnion=length2;
-    }
-    void Intersection (int set1[], int set2[], int setResultant[]){
+}
+void Union(int set1[],int set2[], int setResultant[]){
+      int size1;
+      int size2;
+      setResultant[0]=-1;
+      Cardinality(set2,&size2);
+      Cardinality(set1,&size1);
+      int sizeUnion;
+      if(size1>size2) {
+          sizeUnion = size1;
+          int i,k;
+          for (i=0; i<size1; i++){
+              Add(set1[i],setResultant);
+          }
+          for (i=0; i<sizeUnion; i++){
+              for(k=0; k<size2; k++){
+                  if(setResultant[i]!=set2[k]) Add(set2[k], setResultant);
+              }
+          }
+      }
+      if(size2>size1) {
+          sizeUnion = size2;
+          int j,l;
+          for (j=0; j<size2; j++){
+              Add(set2[j], setResultant);
+          }
+          for (j=0; j<sizeUnion; j++){
+              for(l=0; l<size1; l++){
+                  if(setResultant[j]!=set1[l]) Add(set1[l], setResultant);
+              }
+          }
 
+      }
 }
 bool Empty(int set[]){
     int size;
@@ -70,7 +100,7 @@ bool Element(int x, int set[]){
     int n=0;
     int size;
     Cardinality(set, &size);
-    if (x<1 || x>4095) return false;
+    if (Universe(x)) return false;
     else {
         while(n<size-1 && x!=set[n]){
             if (x==set[n]) return true;
@@ -81,7 +111,7 @@ bool Element(int x, int set[]){
 }
 double Arithmetic (int set[]){
     int size;
-    double aMean;
+    double aMean=0;
     int n = 0;
     Cardinality (set,&size);
     if (size==0) aMean=0;
@@ -96,7 +126,7 @@ double Arithmetic (int set[]){
 }
 double Harmonic (int set[]){
     int size;
-    double hMean;
+    double hMean=0;
     double hDenominator=0;
     double hNumerator=0;
     int n = 0;
@@ -112,34 +142,65 @@ double Harmonic (int set[]){
     }
     return hMean;
 }
+bool Equal(int set1[], int set2[]){
+    int size1,size2;
+    Cardinality(set1,&size1);
+    Cardinality( set2, &size2);
+    if (size1!=size2) return false;
+    else {
+        bubbleSort(set1);
+        bubbleSort(set2);
+        for (int i = 0; i < size1; i++){
+            if (set1[i]!=set2[i]) return false;
+        }
+    }
+    return true;
+}
+bool Subset(int set1[], int set2[]){
+    int size1,size2;
+    Cardinality(set1,&size1);
+    Cardinality(set2,&size2);
+    int j, i,k=0;
+    if(size1>size2){
+        for (i=0;i<size2;i++){
+            for(j=0;j<size1;j++){
+                if(set2[i]==set1[j]) k++;
+            }
+        }
+        if (k==size2) return true;
+        else return false;
+    }
+    if(size1<size2){
+        for(i=0;i<size1;i++){
+            for(j=0;j<size2;j++){
+                if(set1[i]==set2[j]) k++;
+            }
+        }
+        if(k==size1) return true;
+        else return false;
+    }
+    if(size1==size2) Equal(set1,set2);
+}
 int main() {
-    int array[5] = {4, 3, 2, 1, -1};
-    int arraytest[1]{-1};
-    int sizetest;
-    int arraytest1[4] = {1,2,5000,3} ;
-    int settest1[1];
-    int setsize;
-    int number1 = 6;
-    int maximum;
-    int minimum;
-    Add(number1, array);
-    int z;
-    for (z = 0; z < 6; z++) {
-        std::cout << array[z] << " ";
-    }
-    Cardinality(array, &setsize);
-    Cardinality(arraytest, &sizetest);
-    std::cout << std::endl;
-    std::cout << setsize;
-    std::cout << std::endl;
-    std::cout << sizetest << std::endl;
-    std::cout << Harmonic(array) << std::endl;
-    std::cout << Arithmetic(array) << std::endl;
-    MinMax(array, &minimum, maximum);
-    std::cout << minimum << " " << maximum << std::endl;
-    Create(3, arraytest1, settest1);
-    for (z = 0; z < 3; z++) {
-        std::cout << settest1[z] << " ";
-    }
+    int tab[5]={6,7,100,200,300};
+    int creation[0];
+    int array1[3]={1,2,-1};
+    int array2[4]={1,2,3,-1};
+    int array3[3]={1,3,-1};
+    int array4[3]={1,2,-1};
+    std::cout<<Equal(array1,array2)<<std::endl;
+    std::cout<<Equal(array1,array3)<<std::endl;
+    std::cout<<Equal(array1,array4)<<std::endl;
+    int size1,size2,size3,size4;
+    Cardinality(array1,&size1);
+    Cardinality(array2,&size2);
+    Cardinality(array3,&size3);
+    Cardinality(array4,&size4);
+    std::cout << size1 << " " << size2 << " " << size3 << " " << size4 << " " << Subset(array1,array4)<< std::endl;
+    Add(6, array1);
+    Add(7, array1);
+    std::cout << array1[2]<<" "<<array1[3]<<" "<<array1[4]<<std::endl;
+    Create(5,tab,creation);
+    std::cout << creation[0] << creation[2]<<creation[5];
     return 0;
 }
