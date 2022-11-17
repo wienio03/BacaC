13696e650cdd66375485b6244f514dd4bbe18831
zmiana <wienczyslaw.wlodyga@student.uj.edu.pl>
2022-11-17 20:54:12 +0100

    zmiana 16_11 23:17

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 //Wieńczysław Włodyga
 #include <iostream>
-
 bool Universe(int x){
     if (x<=4095 && x>=1) return true;
     else return false;
@@ -17,7 +16,7 @@ void bubbleSort(int set[]) {
     int i,j;
     for (i=0; i<size; i++){
         for (j=1; j<size; j++){
-            if (set[i]<set[j]) std::swap(set[i],set[j]);
+            if (set[i]>set[j]) std::swap(set[i],set[j]);
         }
     }
 }
@@ -39,51 +38,20 @@ void MinMax(int set[], int* min, int &max){
 void Add(int number, int set[]){
     int n;
     Cardinality(set,&n);
-    if (number>=1 && number <=4095){
+    if (Universe(number)){
         n++;
     set[n-1]=number;
     set[n]=(-1);
     }
 }
 void Create(unsigned int usedElements, int sourceSet[], int set[]){
+*set=-1;
 int i;
 for (i=0; i<usedElements; i++){
     if(Universe(sourceSet[i]))Add(sourceSet[i],set);
 }
 }
-void Union(int set1[],int set2[], int setResultant[]){
-      int size1;
-      int size2;
-      setResultant[0]=-1;
-      Cardinality(set2,&size2);
-      Cardinality(set1,&size1);
-      int sizeUnion;
-      if(size1>size2) {
-          sizeUnion = size1;
-          int i,k;
-          for (i=0; i<size1; i++){
-              Add(set1[i],setResultant);
-          }
-          for (i=0; i<sizeUnion; i++){
-              for(k=0; k<size2; k++){
-                  if(setResultant[i]!=set2[k]) Add(set2[k], setResultant);
-              }
-          }
-      }
-      if(size2>size1) {
-          sizeUnion = size2;
-          int j,l;
-          for (j=0; j<size2; j++){
-              Add(set2[j], setResultant);
-          }
-          for (j=0; j<sizeUnion; j++){
-              for(l=0; l<size1; l++){
-                  if(setResultant[j]!=set1[l]) Add(set1[l], setResultant);
-              }
-          }
 
-      }
-}
 bool Empty(int set[]){
     int size;
     Cardinality(set,&size);
@@ -100,14 +68,54 @@ bool Element(int x, int set[]){
     int n=0;
     int size;
     Cardinality(set, &size);
-    if (Universe(x)) return false;
-    else {
-        while(n<size-1 && x!=set[n]){
+    if (!Universe(x)) return false;
+        while(n<size){
             if (x==set[n]) return true;
             n++;
-            if(n==size-1) return false;
+            if(n==size) return false;
         }
     }
+void Union(int set1[],int set2[], int setResultant[]){
+    int size1;
+    int size2;
+    *setResultant=-1;
+    Cardinality(set2,&size2);
+    Cardinality(set1,&size1);
+    int i,k;
+    for (i=0; i<size1; i++){
+        Add(set1[i],setResultant);
+    }
+        for(k=0; k<size2; k++){
+            if(!Element(set2[k],setResultant)) Add(set2[k], setResultant);
+        }
+}
+void Intersection(int set1[],int set2[], int setIntersection[]){
+    int size1;
+    *setIntersection=-1;
+    Cardinality(set1,&size1);
+    int i;
+    for(i=0; i<size1; i++){
+        if(Element(set1[i],set2)) Add(set1[i],setIntersection);
+    }
+}
+void Difference(int set1[],int set2[], int setDifference[]){
+    *setDifference=-1;
+    int size1;
+    Cardinality(set1, &size1);
+    int i;
+    for(i=0; i<size1;i++){
+        if(!Element(set1[i],set2)) Add(set1[i],setDifference);
+    }
+}
+void Complement(int set[], int setComplement[]){
+    *setComplement=-1;
+    int i;
+    for(i=1; i<4096; i++){
+        if(!Element(i,set)) Add(i,setComplement);
+        }
+}
+void Symetric(int set1[], int set2[], int setSymetric[]){
+
 }
 double Arithmetic (int set[]){
     int size;
@@ -161,6 +169,7 @@ bool Subset(int set1[], int set2[]){
     Cardinality(set1,&size1);
     Cardinality(set2,&size2);
     int j, i,k=0;
+    if(size1==0 || size2==0) return true;
     if(size1>size2){
         for (i=0;i<size2;i++){
             for(j=0;j<size1;j++){
@@ -181,26 +190,31 @@ bool Subset(int set1[], int set2[]){
     }
     if(size1==size2) Equal(set1,set2);
 }
+void Properties(int set[], char sign, double &arithmetic, double* harmonic, int &max, int* min, int &cardinality) {
+}
+
 int main() {
     int tab[5]={6,7,100,200,300};
-    int creation[0];
+    int creation[5];
     int array1[3]={1,2,-1};
     int array2[4]={1,2,3,-1};
     int array3[3]={1,3,-1};
     int array4[3]={1,2,-1};
-    std::cout<<Equal(array1,array2)<<std::endl;
-    std::cout<<Equal(array1,array3)<<std::endl;
-    std::cout<<Equal(array1,array4)<<std::endl;
-    int size1,size2,size3,size4;
-    Cardinality(array1,&size1);
-    Cardinality(array2,&size2);
-    Cardinality(array3,&size3);
-    Cardinality(array4,&size4);
-    std::cout << size1 << " " << size2 << " " << size3 << " " << size4 << " " << Subset(array1,array4)<< std::endl;
-    Add(6, array1);
-    Add(7, array1);
-    std::cout << array1[2]<<" "<<array1[3]<<" "<<array1[4]<<std::endl;
-    Create(5,tab,creation);
-    std::cout << creation[0] << creation[2]<<creation[5];
+    int arrayunion[5];
+    int arrayintersection[5];
+    int arraydifference[5];
+    int arraycomplement[4095];
+    Difference(array1,array3,arraydifference);
+    Union(array1,array3,arrayunion);
+    for(int j = 0; j<4;j++ ){
+    std::cout << arrayunion[j]<<" ";}
+    std::cout << std::endl;
+    for(int j = 0; j<3;j++ ){
+        std::cout << arraydifference[j]<<" ";}
+    std::cout << std::endl;
+    Complement(array1,arraycomplement);
+    for(int j = 0; j < 4094; j++){
+        std::cout << arraycomplement[j]<<" ";
+    }
     return 0;
 }
